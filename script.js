5a3de92c5337401a048b0ee4791f1b34f3310629
Mario Andres Doncel Neto <88mario.doncel@gmail.com>
2021-03-19 14:35:35 -0300

    Snake criada

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 let canvas = document.getElementById('canvas')
 let context = canvas.getContext('2d')
 let box = 32
+let snake = []
+snake[0] = {
+    x:8*box,
+    y:8*box
+}
 
-function criarBG() {
+function createBG() {
     context.fillStyle = 'lightgreen'
     context.fillRect(0,0,16*box,16*box)   
 }
+function createSnake() {
+    snake.forEach(element => {
+        context.fillStyle = "green"
+        context.fillRect(element.x, element.y, box, box)
+    });
+}
 
-criarBG()
+createBG()
+createSnake()
