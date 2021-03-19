cf7c705264f49f4ed961174bbee9beb3a496507c
Mario Andres Doncel Neto <88mario.doncel@gmail.com>
2021-03-19 14:51:22 -0300

    Moviment created

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ let canvas = document.getElementById('canvas')
 let context = canvas.getContext('2d')
 let box = 32
 let snake = []
-snake[0] = {
-    x:8*box,
-    y:8*box
-}
-
+    snake[0] = {
+        x:8*box,
+        y:8*box
+    }
+let direction = "right"
 function createBG() {
     context.fillStyle = 'lightgreen'
     context.fillRect(0,0,16*box,16*box)   
@@ -18,5 +18,35 @@ function createSnake() {
     });
 }
 
-createBG()
-createSnake()
+function playGame() {
+    createBG()
+    createSnake()
+
+    let snakeX = snake[0].x
+    let snakeY = snake[0].y
+
+    switch (direction) {
+        case "right":
+            snakeX += box
+            break;
+        case "left":
+            snakeX -= box
+            break;
+        case "down":
+            snakeY += box
+            break;
+        case "up":
+            snakeY -= box
+            break;
+        default:
+            break;
+    }
+    snake.pop()
+    let newHead = {
+        x:snakeX,
+        y:snakeY
+    }
+    snake.unshift(newHead)
+}
+let game = setInterval(playGame, 100)
+
