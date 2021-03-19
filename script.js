1e13d281f3402dcc968485fc6f866814346c2a5e
Mario Andres Doncel Neto <88mario.doncel@gmail.com>
2021-03-19 15:20:11 -0300

    Tela infinita implementada

--- script.js
+++ script.js
@@ -17,11 +17,26 @@ function createSnake() {
         context.fillRect(element.x, element.y, box, box)
     });
 }
+function changeDirection(event) {
+    if (event.keyCode == 37 && direction != 'right') direction ="left"
+    if (event.keyCode == 39 && direction != 'left') direction ="right"
+    if (event.keyCode == 40 && direction != 'up') direction ="down"
+    if (event.keyCode == 38 && direction != 'down') direction ="up"
+}
+
+document.addEventListener('keydown', changeDirection)
 
 function playGame() {
+    
+    if(snake[0].x > 15*box && direction == "right") snake[0].x = 0
+    if(snake[0].x < 0*box && direction == "left") snake[0].x = 16*box
+    if(snake[0].y > 15*box && direction == "down") snake[0].y = 0
+    if(snake[0].y < 0*box && direction == "up") snake[0].y = 16*box
+
     createBG()
     createSnake()
 
+    // Initial position
     let snakeX = snake[0].x
     let snakeY = snake[0].y
 
@@ -41,12 +56,20 @@ function playGame() {
         default:
             break;
     }
-    snake.pop()
-    let newHead = {
+    
+    let newPosition = {
         x:snakeX,
         y:snakeY
     }
-    snake.unshift(newHead)
+
+    // Add snake in new position
+    snake.unshift(newPosition)
+    // Delete snake in the last position
+    snake.pop()
+
+    
 }
+
+
 let game = setInterval(playGame, 100)
 
